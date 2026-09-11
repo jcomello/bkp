77205f594c64b16e33cdf769a4bf00c65d7a1241
João Mello <joaocarreira88@gmail.com>
2026-09-11 16:25:26 -0300

    Set main options and arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use anyhow::{Error, anyhow};
+use clap::Parser;
+use std::path::Path;
+
+#[derive(Parser)]
+#[command(name = "bkp")]
+#[command(version = "0.1.0")]
+#[command(about = "Creates backup for single files", long_about = None)]
+struct Cli {
+    /// File path to be backed up
+    #[arg(value_parser = validate_path)]
+    path: String,
+
+    /// Changes the date format for the backup file
+    #[arg(long, short, default_value_t = "%Y%m%d".to_string())]
+    format: String,
+}
+
+fn validate_path(s: &str) -> Result<String, Error> {
+    let path = Path::new(s);
+
+    match path.try_exists() {
+        Ok(true) => Ok(s.to_string()),
+        Ok(false) => Err(anyhow!("Path doesn't exists")),
+        Err(e) => Err(e.into()),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("path: {:?}", cli.path);
+    println!("format: {:?}", cli.format);
 }
