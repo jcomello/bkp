1e0932ee78b94542b1736bb95c8c0dc9e032805a
João Mello <joaocarreira88@gmail.com>
2026-09-14 16:14:27 -0300

    Creates the backup file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{Error, anyhow};
 use clap::Parser;
 use std::path::Path;
+use std::fs;
+use chrono::Utc;
 
 #[derive(Parser)]
 #[command(name = "bkp")]
@@ -12,7 +14,7 @@ struct Cli {
     path: String,
 
     /// Changes the date format for the backup file
-    #[arg(long, short, default_value_t = "%Y%m%d".to_string())]
+    #[arg(long, short, default_value_t = "%Y%m%d%H%M".to_string())]
     format: String,
 }
 
@@ -28,7 +30,9 @@ fn validate_path(s: &str) -> Result<String, Error> {
 
 fn main() {
     let cli = Cli::parse();
+    let source = &cli.path;
+    let now = Utc::now().format(&cli.format);
+    let destination = format!("{source}.{now}.bkp");
 
-    println!("path: {:?}", cli.path);
-    println!("format: {:?}", cli.format);
+    let _ = fs::copy(cli.path, destination);
 }
